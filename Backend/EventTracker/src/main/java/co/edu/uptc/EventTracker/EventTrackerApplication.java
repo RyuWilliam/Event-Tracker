9f9a8cfa819cf236c3b12d91956f1e308395de71
William González <ruyk1120@gmail.com>
2026-02-16 22:06:32 -0500

    First initial configuration and mapstruct dependency added

---
+++ Backend/EventTracker/src/main/java/co/edu/uptc/EventTracker/EventTrackerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.EventTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTrackerApplication.class, args);
+	}
+
+}
---
+++ Backend/EventTracker/src/test/java/co/edu/uptc/EventTracker/EventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.EventTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
