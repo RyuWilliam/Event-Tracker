1bad6aa2aacfe02f05b3650d562ebfa80eeea065
William González <ruyk1120@gmail.com>
2026-02-20 10:23:49 -0500

    Test folder for database correct working deleted

--- Backend/EventTracker/src/main/java/co/edu/uptc/EventTracker/test/DummyRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uptc.EventTracker.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface DummyRepo extends JpaRepository<DummyEntity, Integer> {
-
-}
