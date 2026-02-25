3d43ebab157a0e979cd0ff4392b1dd74307608e6
William González <ruyk1120@gmail.com>
2026-02-24 23:43:21 -0500

    feat: generate ordered report by likes

---
+++ Backend/EventTracker/src/main/java/co/edu/uptc/EventTracker/domain/service/ReportService.java
@@ -0,0 +1,34 @@
+package co.edu.uptc.EventTracker.domain.service;
+
+
+import co.edu.uptc.EventTracker.domain.model.Event;
+import co.edu.uptc.EventTracker.domain.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ReportService {
+
+    private final EventRepository eventRepository;
+
+    public ReportService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Map<String, Integer> generateReport(){
+        List<Event> events = new ArrayList<>(eventRepository.findActive());
+        Map<String, Integer> report = new LinkedHashMap<>();
+        while(!events.isEmpty()) {
+            Event maximum = events.getFirst();
+            for (Event event : events) {
+                if (event.getLikes() > maximum.getLikes()) {
+                    maximum = event;
+                }
+            }
+            report.put(maximum.getName(), maximum.getLikes());
+            events.remove(maximum);
+        }
+        return report;
+    }
+}
