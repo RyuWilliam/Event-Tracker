e39c58164fc63af93909c8e2a9d66a16f5b4448d
Santino33 <davidnaranjo330@gmail.com>
2026-04-22 14:36:47 -0500

    refactor: move ExternalEventCard and EventDetailsDialog to events feature

--- Frontend/src/features/external/index.ts
+++ Frontend/src/features/external/index.ts
@@ -1,4 +1,2 @@
-export { ExternalEventCard } from "./components/ExternalEventCard"
-export { EventDetailsDialog } from "./components/EventDetailsDialog"
 export { ExternalEventsPage } from "./pages/ExternalEventsPage"
 export { FavoritesPage } from "./pages/FavoritesPage"
